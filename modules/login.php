11e6b86650927aa7d11d787c856887d0d74f2acd
Clement Defretiere <clement.def.7@gmail.com>
2019-03-31 17:28:09 +0200

    login page designed

--- modules/login.php
+++ modules/login.php
@@ -1,4 +1,22 @@
-<form>
-    <input type="text" name="username">
-    <input type="password" name="password">
-</form>
+<link rel="stylesheet" type="text/css" href="modules/css/login.css">
+
+<div class="wrapper">
+    <div id="logo">
+        <img src="/images/logo.png" alt="Logo PinIt!"/>
+    </div>
+
+    <div id="form-login-holder">
+        <form id="form-login" method="POST">
+            <div id="avatar">
+                <img alt="avatar" src="/images/clem.jpg">
+            </div>
+            <label for="username">Username</label>
+            <input type="text" class="input" id="username" name="username">
+            <br>
+            <label for="password">Password</label>
+            <input type="password" class="input" id="password" name="password">
+            <br>
+            <input type="submit" class="btn" value="Login">
+        </form>
+    </div>
+</div>
