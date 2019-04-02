c7137f33bc3511e8572be1bc53b9645ca88fbad5
Julie Payen <julie.payen19@gmail.com>
2019-04-02 19:57:57 +0200

    mobile adaptation

--- modules/head.php
+++ modules/head.php
@@ -1,5 +1,6 @@
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=no">
     <title>PinIt!</title>
     
     <link rel="stylesheet" type="text/css" href="modules/css/head.css">
